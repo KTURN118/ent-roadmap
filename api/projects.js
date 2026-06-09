c54b0e95d1dea90bdd59661c09ee851afdcf058a
KTURN118 <kingsley.turner@entelligence.com>
2026-06-09 16:12:33 -0400

    Update projects.js

--- api/projects.js
+++ api/projects.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': apiKey
+        'Authorization': `Bearer ${apiKey}`
       },
       body: JSON.stringify({ query })
     });
